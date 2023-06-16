e6877fe29fcc4997396c0323c3b501d1fa152166
Sunjay Cauligi <sunjay.cauligi@mpi-sp.org>
2023-06-17 01:49:39 +0530

    vestigial

--- hy/importer.py
+++ hy/importer.py
@@ -176,14 +176,6 @@ del sys.modules["runpy"]
 
 
 
-def _import_from_path(name, path):
-    """A helper function that imports a module from the given path."""
-    spec = importlib.util.spec_from_file_location(name, path)
-    mod = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(mod)
-    return mod
-
-
 def _inject_builtins():
     """Inject the Hy core macros into Python's builtins if necessary"""
     if hasattr(builtins, "__hy_injected__"):
